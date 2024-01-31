facd9e29e1b06f9a028c493100896612e0330ea1
Jay Patel <jupatel1104@gmail.com>
2024-01-31 09:23:57 -0500

    dynamic data into resturant cards using props

--- App.js
+++ App.js
@@ -28,18 +28,24 @@ const Header = () => {
   );
 };
 
-const ResturantCard = () => {
+const ResturantCard = (props) => {
   return (
     <div className="rest-card">
-      <h3 className="rest-name"></h3>
+      <h1 className="rest-name"> {props.name} </h1>
+
       <img
-        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ika9k8qszplblbccmqcs"
+        src={props.imgSource}
         alt="Meghana Foods image"
         className="rest-img"
       />
-      <h3 className="rest-name">Biryani, North Indian, Asian</h3>
-      <h3 className="rest-name">4.3 Stars</h3>
-      <h3 className="rest-name">38 minutes</h3>
+      <h3 className="rest-name">{props.cuisine}</h3>
+      <h3 className="rest-name">
+        <i className="fa-regular fa-star" id="icon-star"></i>4.3 Stars
+      </h3>
+      <h3 className="rest-name">
+        <i className="fa-regular fa-clock" id="icon-timer"></i>
+        {props.delTime}
+      </h3>
     </div>
   );
 };
@@ -63,22 +69,65 @@ const Body = () => {
       <SearchBar></SearchBar>
 
       <div className="rest-container">
-        <ResturantCard Starbucks />
-        {/* https://mcdonough.com/wp-content/uploads/2020/09/starbucks-logo-png-transparent.png */}
-        <ResturantCard Dominos Pizza />
-        {/* https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Domino%27s_pizza_logo.svg/2036px-Domino%27s_pizza_logo.svg.png */}
-        <ResturantCard Burger King />
-        {/* https://cdn.freebiesupply.com/images/large/2x/burger-king-logo-png-transparent.png */}
-        <ResturantCard Tim Hortons />
-        {/* https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjR7SoKEYqzAxY9koSsVBHlBfSHRUOs5J_47jebp50rRtanT77Hk1lQBN569uQQvpYNFY&usqp=CAU */}
-        <ResturantCard Boston Pizza />
-        {/* https://upload.wikimedia.org/wikipedia/en/2/20/Boston_Pizza.svg */}
-        <ResturantCard Subway />
-        {/* https://cdn.freebiesupply.com/logos/large/2x/subway-11-logo-png-transparent.png */}
-        <ResturantCard Ginos Pizza />
-        {/* https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQCS66oTaCNZ7xduIzTM7gx1Hb5yrEl0rPpvoSWX2Zdw&s */}
-        <ResturantCard Wendys />
-        {/* https://assets.stickpng.com/thumbs/58429d58a6515b1e0ad75ae8.png */}
+        <ResturantCard
+          restName="Starbucks"
+          cuisine="Ham & Swiss on Baguette , Chicken & Bacon on Brioche "
+          imgSource="https://mcdonough.com/wp-content/uploads/2020/09/starbucks-logo-png-transparent.png"
+          rating="4.4 Stars"
+          delTime="40 minutes"
+        />
+        <ResturantCard
+          restName="Dominos Pizza"
+          cuisine="Order Domino's pizza, pasta, sandwiches & more"
+          imgSource="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Domino%27s_pizza_logo.svg/2036px-Domino%27s_pizza_logo.svg.png"
+          rating="4.2 Stars"
+          delTime="25 minutes"
+        />
+        <ResturantCard
+          restName="Burger King"
+          cuisine="Whopper patty, topped with cheese, thick-cut smoked bacon"
+          imgSource="https://logowik.com/content/uploads/images/310_burgerking.jpg"
+          rating="3.2 Stars"
+          delTime="55 minutes"
+        />
+        <ResturantCard
+          restName="Tim Hortons"
+          cuisine="Home of Canada's favourite coffee"
+          imgSource="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjR7SoKEYqzAxY9koSsVBHlBfSHRUOs5J_47jebp50rRtanT77Hk1lQBN569uQQvpYNFY&usqp=CAU"
+          rating="4.0 Stars"
+          delTime="15 minutes"
+        />
+        <ResturantCard
+          restName="Boston Pizza"
+          cuisine="pizza, pasta, burgers, appetizers, salads and more"
+          imgSource="https://upload.wikimedia.org/wikipedia/en/2/20/Boston_Pizza.svg"
+          rating="5.0 Stars"
+          delTime="28 minutes"
+        />
+
+        <ResturantCard
+          restName="Subway"
+          cuisine="submarine sandwiches (subs), wraps, salads, and drinks."
+          imgSource="https://cdn.freebiesupply.com/logos/large/2x/subway-11-logo-png-transparent.png"
+          rating="4.9 Stars"
+          delTime="35 minutes"
+        />
+
+        <ResturantCard
+          restName="Ginos Pizza"
+          cuisine="Pizza resturant"
+          imgSource="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQCS66oTaCNZ7xduIzTM7gx1Hb5yrEl0rPpvoSWX2Zdw&s"
+          rating="3.5 Stars"
+          delTime="45 minutes"
+        />
+
+        <ResturantCard
+          restName="Wendys"
+          cuisine="Chicken Nuggets, Beaf, Eggs"
+          imgSource="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2w3HAVufZroDs8DBYwBq2ofwml6a0t2BFIyDFjigtoA&s"
+          rating="5.0 Stars"
+          delTime="65 minutes"
+        />
       </div>
     </div>
   );
