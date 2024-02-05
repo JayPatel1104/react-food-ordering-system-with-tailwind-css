8fa03ea6a4b2d2f61bf802c49c5ceba1292e570d
Jay Patel <jupatel1104@gmail.com>
2024-02-05 13:59:47 -0500

    looping over resturant list

--- App.js
+++ App.js
@@ -961,77 +961,9 @@ const Body = () => {
       <SearchBar></SearchBar>
 
       <div className="rest-container">
-        <ResturantCard resData={resList[0]}></ResturantCard>
-        <ResturantCard resData={resList[1]}></ResturantCard>
-        <ResturantCard resData={resList[2]}></ResturantCard>
-        <ResturantCard resData={resList[3]}></ResturantCard>
-        <ResturantCard resData={resList[4]}></ResturantCard>
-        <ResturantCard resData={resList[5]}></ResturantCard>
-        <ResturantCard resData={resList[6]}></ResturantCard>
-        <ResturantCard resData={resList[7]}></ResturantCard>
-        <ResturantCard resData={resList[8]}></ResturantCard>
-        <ResturantCard resData={resList[10]}></ResturantCard>
-        <ResturantCard resData={resList[11]}></ResturantCard>
-
-        {/* <ResturantCard
-          restName="Starbucks"
-          cuisine="Ham & Swiss on Baguette , Chicken & Bacon on Brioche "
-          imgSource="https://mcdonough.com/wp-content/uploads/2020/09/starbucks-logo-png-transparent.png"
-          rating="4.4 Stars"
-          delTime="40 minutes"
-        />
-        <ResturantCard
-          restName="Dominos Pizza"
-          cuisine="Order Domino's pizza, pasta, sandwiches & more"
-          imgSource="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Domino%27s_pizza_logo.svg/2036px-Domino%27s_pizza_logo.svg.png"
-          rating="4.2 Stars"
-          delTime="25 minutes"
-        />
-        <ResturantCard
-          restName="Burger King"
-          cuisine="Whopper patty, topped with cheese, thick-cut smoked bacon"
-          imgSource="https://logowik.com/content/uploads/images/310_burgerking.jpg"
-          rating="3.2 Stars"
-          delTime="55 minutes"
-        />
-        <ResturantCard
-          restName="Tim Hortons"
-          cuisine="Home of Canada's favourite coffee"
-          imgSource="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjR7SoKEYqzAxY9koSsVBHlBfSHRUOs5J_47jebp50rRtanT77Hk1lQBN569uQQvpYNFY&usqp=CAU"
-          rating="4.0 Stars"
-          delTime="15 minutes"
-        />
-        <ResturantCard
-          restName="Boston Pizza"
-          cuisine="pizza, pasta, burgers, appetizers, salads and more"
-          imgSource="https://upload.wikimedia.org/wikipedia/en/2/20/Boston_Pizza.svg"
-          rating="5.0 Stars"
-          delTime="28 minutes"
-        />
-
-        <ResturantCard
-          restName="Subway"
-          cuisine="submarine sandwiches (subs), wraps, salads, and drinks."
-          imgSource="https://cdn.freebiesupply.com/logos/large/2x/subway-11-logo-png-transparent.png"
-          rating="4.9 Stars"
-          delTime="35 minutes"
-        />
-
-        <ResturantCard
-          restName="Ginos Pizza"
-          cuisine="Pizza resturant"
-          imgSource="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQCS66oTaCNZ7xduIzTM7gx1Hb5yrEl0rPpvoSWX2Zdw&s"
-          rating="3.5 Stars"
-          delTime="45 minutes"
-        />
-
-        <ResturantCard
-          restName="Wendys"
-          cuisine="Chicken Nuggets, Beaf, Eggs"
-          imgSource="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2w3HAVufZroDs8DBYwBq2ofwml6a0t2BFIyDFjigtoA&s"
-          rating="5.0 Stars"
-          delTime="65 minutes"
-        /> */}
+        {resList.map((resturant) => (
+          <ResturantCard resData={resturant} />
+        ))}
       </div>
     </div>
   );
